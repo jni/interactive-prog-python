dcd7cba2fc000aeecd37990e8bad6a5408d322ff
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2015-10-11 17:51:02 +1100

    Improve code readability

--- memory.py
+++ memory.py
@@ -9,7 +9,8 @@ TEXT_SIZE = 64  # in points
 
 # helper function to initialize globals
 def new_game():
-    global cards, exposed, state, current, score, turns,\
+    # global variables to hold game state
+    global cards, exposed, state, current, turns,\
            lastcard, label, tohide
     score = 0
     turns = 0
@@ -24,27 +25,31 @@ def new_game():
 
 # define event handlers
 def mouseclick(pos):
-    global cards, exposed, state, current, score, turns,\
+    global cards, exposed, state, current, turns,\
            lastcard, label, tohide
     index = pos[0] // CARD_WIDTH
     if not exposed[index]:
         print 'clicked on card number', index
         exposed[index] = True
+        # hide unmatched cards from previous turn, if any
         for h in tohide:
             exposed[h] = False
+        # and reset the list
         tohide = []
     else:
         print 'click IGNORED on card number', index
         return
     currentcard = (index, cards[index])
-    if state == 1:
+    if state == 1:  #  step number in current turn
         state = 0
         turns += 1
         label.set_text('Turns = %i' % turns)
         if lastcard[1] == currentcard[1]:
-            score += 1
+            # the cards match and we are awesome
             lastcard = (-1, -1)
         else:
+            # Oops, they didn't match
+            # Hide at start of next turn
             tohide = [lastcard[0], currentcard[0]]
     else:
         state = 1
@@ -55,6 +60,8 @@ def mouseclick(pos):
 def draw(canvas):
     global cards, exposed
     for i, (exp, val) in enumerate(zip(exposed, cards)):
+        # Rectangle to be drawn. Coordinates are in pixels,
+        # (width, height), and origin is at top-left
         lower_left = i * CARD_WIDTH, 100
         upper_left = i * CARD_WIDTH, 0
         upper_right = (i + 1) * CARD_WIDTH, 0
